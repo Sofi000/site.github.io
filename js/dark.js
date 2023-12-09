ee7a391ab921a50455b4ef98ca6945fdb94caeeb
Dani <129549613+Sofi000@users.noreply.github.com>
2023-12-09 02:38:21 -0300

    teste

--- js/dark.js
+++ js/dark.js
@@ -1,4 +1,12 @@
-function dark(){
-    var element = document.body;
-    element.classList.toggle("dark");
+var element = document.getElementById('body');
+var dl = new Boolean() ;
+if (element.classList == ("dark-mode")){
+    dl = false;
+}
+else {
+    dl = true;
+}
+if(dl == false){
+        var element = document.body;
+        element.classList.toggle("dark-mode");
 }
