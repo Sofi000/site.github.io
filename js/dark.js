81679cc6e95a97d2fccd753f36e8a34edc93d5c2
Dani <129549613+Sofi000@users.noreply.github.com>
2023-12-09 00:49:05 -0300

    teste

--- js/dark.js
+++ js/dark.js
@@ -3,3 +3,8 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', ()=>{
     document.body.classList.toggle('dark')
 })
+
+function dark(){
+    var element = document.body;
+    element.classList.toggle("dark");
+}
