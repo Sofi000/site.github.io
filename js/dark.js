e39ed00385b03189797f16c357f8a39beb4ab4fe
Dani <129549613+Sofi000@users.noreply.github.com>
2023-12-09 01:01:00 -0300

    ajuste

--- js/dark.js
+++ js/dark.js
@@ -1,9 +1,3 @@
-const chk = document.getElementById('chk')
-
-chk.addEventListener('change', ()=>{
-    document.body.classList.toggle('dark')
-})
-
 function dark(){
     var element = document.body;
     element.classList.toggle("dark");
